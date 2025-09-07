190eeadb0e730376afd6af32b478dd627d3b5123
Artem Stepanenko <alexplay.stepanenko@gmail.com>
2025-09-07 19:30:39 +0300

    update: character rotation module

--- Assets/Scripts/Character/Modules/Rotation/Variants/CharacterRotationModule.cs
+++ Assets/Scripts/Character/Modules/Rotation/Variants/CharacterRotationModule.cs
@@ -4,18 +4,24 @@ namespace Character.Modules.Rotation.Variants
 {
     public class CharacterRotationModule : RotationModule
     {
+        [SerializeField] private float _smoothTime = 0.05f;
         [SerializeField] private float _sensetivity;
         
         [Header("Roots")]
         [SerializeField] private Transform _body;
         [SerializeField] private Transform _camera;
-        
+
+        private Vector2 _currentInput;
+        private Vector2 _inputVelocity;
+
         public override void Rotate(Vector2 input)
         {
-            var computedInput = input.normalized * _sensetivity;
+            _currentInput = Vector2.SmoothDamp(_currentInput, input, ref _inputVelocity, _smoothTime);
             
-            _body.Rotate(new Vector3(0, computedInput.x, 0));
-            _camera.Rotate(new Vector3(0, 0, computedInput.y));
+            var computedInput = _currentInput * _sensetivity * Time.deltaTime;
+
+            _body.Rotate(Vector3.up * computedInput.x);
+            _camera.Rotate(Vector3.right * -computedInput.y);
         }
     }
 }
